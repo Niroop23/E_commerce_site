4b9a6d5ae0aa85266a6104be5754b1977f55edf2
Niroop23 <shanmukhaniroop19@gmail.com>
2025-05-17 01:59:59 +0530

    ..

--- e_commerce/vite.config.js
+++ e_commerce/vite.config.js
@@ -3,22 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vendor': ['react', 'react-dom', 'react-router-dom'],
-          'mui': ['@mui/material', '@mui/icons-material'],
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000,
-    sourcemap: true
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
+  plugins: [react()]
 })
